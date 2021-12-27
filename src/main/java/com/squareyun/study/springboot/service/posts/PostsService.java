907b7633006509cd7f8b53e7ce336b1c8e651c06
squareyun <squareyun@gmail.com>
2021-12-27 22:32:18 +0900

    feat: 수정, 조회 API

--- src/main/java/com/squareyun/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/squareyun/study/springboot/service/posts/PostsService.java
@@ -4,8 +4,11 @@ Service Layer는 트랜잭션, 도메인 기능 간의 순서를 보장
 
 package com.squareyun.study.springboot.service.posts;
 
+import com.squareyun.study.springboot.domain.posts.Posts;
 import com.squareyun.study.springboot.domain.posts.PostsRepository;
+import com.squareyun.study.springboot.web.dto.PostsResponseDto;
 import com.squareyun.study.springboot.web.dto.PostsSaveRequestDto;
+import com.squareyun.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class PostsService {
-    
+
     // Autowired가 없네? 이유는 생성자로 의존성 주입을 받기 때문
     // 롬복을 사용하게 되면 해당 클래스의 의존성 관계가 변경되어도 코드 변경이 필요없음
     private final PostsRepository postsRepository;
@@ -21,5 +24,25 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        // update 기능에서 데이터베이스에 쿼리를 날리는 부분이 없다. 이것은 JPA의 영속성 컨텍스트 때문이다.
+        // 더티 체킹으로 트랜잭션이 끝나는 시점에 변화가 있는 모든 엔티티 객체를 디비에 자동 반영해준다. (https://jojoldu.tistory.com/415)
+
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/squareyun/study/springboot/web/PostsApiController.java
+++ src/main/java/com/squareyun/study/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.squareyun.study.springboot.web;
 
 import com.squareyun.study.springboot.service.posts.PostsService;
+import com.squareyun.study.springboot.web.dto.PostsResponseDto;
 import com.squareyun.study.springboot.web.dto.PostsSaveRequestDto;
+import com.squareyun.study.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor // 선언된 모든 final 필드가 포함된 생성자를 생성. final이 없는 필드는 생성자에 포함되지 않음
 @RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
@@ -16,5 +16,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    // Put과 Post의 차이? 멱등성. 동일한 요청을 한 번 보내는 것과 여러 번 연속으로 보내는 것이 같은 효과를 지님
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
