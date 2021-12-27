e5d1cbcc51951a925ff74efcf435abd0655e9cd5
squareyun <squareyun@gmail.com>
2021-12-27 22:46:49 +0900

    feat: 등록, 수정 시간 자동화

--- src/main/java/com/squareyun/study/springboot/Application.java
+++ src/main/java/com/squareyun/study/springboot/Application.java
@@ -1,10 +1,14 @@
+/*
+ 프로젝트의 메인 클래스
+ 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 자동 설정. 이 위치부터 설정을 읽기 때문에 항상 프로젝트의 최상단에 위치해야함.
+ */
 package com.squareyun.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// 프로젝트의 메인 클래스
-// 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 자동 설정. 이 위치부터 설정을 읽기 때문에 항상 프로젝트의 최상단에 위치해야함.
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/squareyun/study/springboot/domain/posts/Posts.java
+++ src/main/java/com/squareyun/study/springboot/domain/posts/Posts.java
@@ -17,7 +17,7 @@ import javax.persistence.*;
 @Getter // 클래스 내 모든 필드의 Getter 메소드를 자동생성
 @NoArgsConstructor // 기본 생성자 자동 추가
 @Entity // 테이블과 링크될 클래스임을 나타냄. 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 테이블의 PK 필드
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성 규칙. IDENTITY 옵션은 auto_increment
