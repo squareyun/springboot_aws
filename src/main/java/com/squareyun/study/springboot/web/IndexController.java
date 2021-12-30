46c1a772928f4731d440c5a8f5961da27d89ef39
squareyun <squareyun@gmail.com>
2021-12-30 10:53:57 +0900

    feat: 게시글 수정 화면, 기능

--- src/main/java/com/squareyun/study/springboot/web/IndexController.java
+++ src/main/java/com/squareyun/study/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.squareyun.study.springboot.web;
 
 import com.squareyun.study.springboot.service.posts.PostsService;
+import com.squareyun.study.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor // 선언된 모든 final 필드가 포함된 생성자를 생성. final이 없는 필드는 생성자에 포함되지 않음
 @Controller
@@ -21,5 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
