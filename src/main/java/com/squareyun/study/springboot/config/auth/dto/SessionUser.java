0bb6236a95f6a3b3e8e56e1d242aa89fbe3ad54d
squareyun <squareyun@gmail.com>
2022-01-04 00:19:44 +0900

    feat: 네이버 로그인 기능

--- src/main/java/com/squareyun/study/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/squareyun/study/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.squareyun.study.springboot.config.auth.dto;
 import com.squareyun.study.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
--- src/main/java/com/squareyun/study/springboot/web/IndexController.java
+++ src/main/java/com/squareyun/study/springboot/web/IndexController.java
@@ -17,14 +17,13 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc()); // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장. 결과를 posts로 index.mustache에 전달
 
         if(user != null) {
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("loginName", user.getName());
         }
 
         return "index";
