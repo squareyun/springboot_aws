722cf1ae12286225accc6d80843b71363fd1bf0b
squareyun <squareyun@gmail.com>
2022-01-05 02:04:25 +0900

    fix: 테스트 코드에 시큐리티 적용

--- src/main/java/com/squareyun/study/springboot/Application.java
+++ src/main/java/com/squareyun/study/springboot/Application.java
@@ -6,9 +6,8 @@ package com.squareyun.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing // JPA Auditing 활성화 // JpaConfig 클래스로 분리
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/squareyun/study/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/squareyun/study/springboot/web/PostsApiControllerTest.java
@@ -1,10 +1,12 @@
 package com.squareyun.study.springboot.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.squareyun.study.springboot.domain.posts.Posts;
 import com.squareyun.study.springboot.domain.posts.PostsRepository;
 import com.squareyun.study.springboot.web.dto.PostsSaveRequestDto;
 import com.squareyun.study.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,12 +40,25 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @After
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_등록된다() throws Exception {
         //given
         String title = "title";
@@ -49,23 +71,37 @@ public class PostsApiControllerTest {
 
         String url = "http://localhost:" + port + "/api/v1/posts";
 
+//        변경 전
+//        //when
+//        /* @WebMvcTest를 사용하지 않고 TestRestTemplate을 사용했음 (전자는 JPA 기능이 작동되지 않기 때문)
+//           @SpringBootTest에서 Web Environment 설정을 했다면 TestRestTemplate는 그에 맞춰서 자동으로 설정되어 빈이 생성됨
+//           MockMvc는 Servlet Container를 생성하지 않는 반면, 이것은 Servlet Container를 사용함. 따라서 실제 서버가 동작하는 것 처럼 테스트 가능
+//         */
+//        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+//
+//        //then
+//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(responseEntity.getBody()).isGreaterThan(0L); // 정상적으로 save 처리 된다면 ID 값이 반환되기 때문에 0보다 클 것
+//
+//        List<Posts> all = postsRepository.findAll();
+//        assertThat(all.get(0).getTitle()).isEqualTo(title);
+//        assertThat(all.get(0).getContent()).isEqualTo(content);
+//
+
         //when
-        /* @WebMvcTest를 사용하지 않고 TestRestTemplate을 사용했음 (전자는 JPA 기능이 작동되지 않기 때문)
-           @SpringBootTest에서 Web Environment 설정을 했다면 TestRestTemplate는 그에 맞춰서 자동으로 설정되어 빈이 생성됨
-           MockMvc는 Servlet Container를 생성하지 않는 반면, 이것은 Servlet Container를 사용함. 따라서 실제 서버가 동작하는 것 처럼 테스트 가능
-         */
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L); // 정상적으로 save 처리 된다면 ID 값이 반환되기 때문에 0보다 클 것
-
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_수정된다() throws Exception {
         //given
         Posts savedPosts = postsRepository.save(Posts.builder()
@@ -85,21 +121,34 @@ public class PostsApiControllerTest {
 
         String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
 
-        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+//        변경전
+//        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+//
+//        //when
+//        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+//
+//        //then
+//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+//
+//        List<Posts> all = postsRepository.findAll();
+//        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+//        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        mvc.perform(put(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
     
     @Test
+    @WithMockUser(roles="USER")
     // 스스로 작성해 본 테스트
     public void Posts_삭제된다() throws Exception {
         //given
@@ -113,15 +162,19 @@ public class PostsApiControllerTest {
 
         String url = "http://localhost:" + port + "/api/v1/posts/" + deleteId;
 
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity entity = new HttpEntity(headers);
+//        변경전
+//        HttpHeaders headers = new HttpHeaders();
+//        HttpEntity entity = new HttpEntity(headers);
+//
+//        //when
+//        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.DELETE, entity, Long.class);
+//
+//        //then
+//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
-        //when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.DELETE, entity, Long.class);
-
-        //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        mvc.perform(delete(url))
+                .andExpect(status().isOk());
 
         List<Posts> all = postsRepository.findAll();
         assertThat(all.size()).isEqualTo(0);
