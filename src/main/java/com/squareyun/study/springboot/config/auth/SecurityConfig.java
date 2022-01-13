e5844ae289796033269912bbdcefe7159b1c8a4b
DESKTOP-2S05CA5\krbes <squareyun@gmail.com>
2022-01-13 23:41:52 +0900

    feat: profile API SecurityConfig 설정 테스트

--- src/main/java/com/squareyun/study/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/squareyun/study/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable().headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 끔
                 .and()
                 .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점. 이걸 선언해야 antMatchers 사용 가능
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() // 권한 관리 대상을 지정하는 옵션
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll() // 권한 관리 대상을 지정하는 옵션
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest() // 설정된 값들 이외 나머지 URL들을 나타냄
                 .authenticated() // 나머지 URL들은 모두 인증된 사용자들에게만 허용
